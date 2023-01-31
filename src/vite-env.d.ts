9dbff5850e3080c834525d801f2c76270113b4a5
Samuel Silva <samuelsilvawb@gmail.com>
2023-01-31 22:21:10 +0000

    chore: enable usage of environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GITHUB_TOKEN: string;
+  readonly VITE_GITHUB_BASE_API: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
